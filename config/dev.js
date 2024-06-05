caed248e3746848b579f682335e0f249ff8b2bf3
Vasyl Boychuk <vasyl.boychuk@panion.org>
2024-06-06 00:27:40 +0300

    adding .env

--- config/dev.js
+++ config/dev.js
@@ -1,7 +1,8 @@
+require('dotenv').config();
+
 module.exports = {
-  googleClientID:
-    "",
-  googleClientSecret: "",
-  mongoURI: "",
-  cookieKey: "",
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  mongoURI: process.env.MONGO_URI,
+  cookieKey: process.env.COOKIE_KEY,
 };

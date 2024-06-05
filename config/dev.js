858a0edcd4ee2bfdc40ea90ba452a34cec48ce38
Vasyl Boychuk <vasyl.boychuk@panion.org>
2024-06-05 23:51:53 +0300

    fix dev

--- config/dev.js
+++ config/dev.js
@@ -1,7 +1,7 @@
 module.exports = {
   googleClientID:
-    "70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com",
-  googleClientSecret: "8mkniDQOqacXtlRD3gA4n2az",
-  mongoURI: "mongodb+srv://vasyaboychuk:ZPBuJsDF4I4VhNG6@cluster0.r14eakv.mongodb.net/advnode?retryWrites=true&w=majority",
-  cookieKey: "123123123",
+    "",
+  googleClientSecret: "",
+  mongoURI: "",
+  cookieKey: "",
 };

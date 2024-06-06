5d09cdd4fc0236a089f221a53093b4f04a192bdb
Vasyl Boychuk <vasyl.boychuk@panion.org>
2024-06-06 18:19:31 +0300

    new implementation

---
+++ jest.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  preset: 'jest-puppeteer',
+  testTimeout: 30000,
+  setupFilesAfterEnv: ["./tests/setup.js"],
+};
--- tests/setup.js
+++ tests/setup.js
@@ -1,5 +1,3 @@
-jest.setTimeout(30000);
-
 require("../models/User");
 
 const mongoose = require("mongoose");
